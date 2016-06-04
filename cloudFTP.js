9f5dca744ba7f34212062cb45d83340680328cb8
Grant Miller <blented@gmail.com>
2016-06-04 13:24:29 -0700

    formatting fixes
    - comments should never be inline w/ code, always on a separate line
    - always a space between the if and the condition
    - never block comment, always use single lines (ctrl+shift+c in sublime)
    - always a space between the comment character and the comment
    - no semicolons
    - assign stuff to variables so the code documents itself
    - try to keep code under 70-80 lines, pretty soft on this one but for
    comments it's a pain to have to scroll to read them
    - single var for each variable, only exception is when you're declaring
    a bunch of undefined variables, ex: var a,b,c,d

--- cloudFTP.js
+++ cloudFTP.js
@@ -1,106 +1,108 @@
-/*
-Basic server implementation using ftpd node module
-*/
 
-/*Modules*/
-var ftpd = require('ftpd'),
-	_ = require('lodash'),
-	fs = require('fs'),
-	config = require('./config/default')
+// Basic server implementation using ftpd node module
 
-/*Constants*/
-var server,
-	options =
-	{
+// Modules
+var ftpd = require('ftpd')
+var _ = require('lodash')
+var fs = require('fs')
+var config = require('./config/default')
+
+// Constants
+var options = {
 		host: config.basics.host,
 		port: config.basics.port,
 	}
 
-/*Server set up*/
-server = new ftpd.FtpServer(options.host,
+// Server set up
+var server = new ftpd.FtpServer(options.host,
+{
+	// No callback, get the initial working directory of
+	// the user, relative to the root directory
+	getInitialCwd: function (connection, callback)
 	{
-		//No callback, get the initial working directory of the user, relative to the root directory
-		getInitialCwd: function (connection, callback)
+		// if Root defined in config file, use that, else,
+		// use local cwd
+
+		// TODO: Store return value
+		var matchingUser = _.find(config.users,
+			{'username': connection.username})
+		if(!matchingUser)
 		{
-			//if Root defined in config file, use that, else, use local cwd
+			var err = new Error('Error: Username not found')
+			console.log(err.message)
+			process.exit(1) //exit with failure
+		}
 
-			//TODO: Store return value
-			if(!_.find(config.users, {'username': connection.username})){
-				var err = new Error('Error: Username not found')
-				console.log(err.message)
-				process.exit(1) //exit with failure
-			}
+		//Parse userDir, from config file and username
+		var userDir = config.basics.root
+		if (config.basics.root)
+		{
+			if (connection.username != 'ingenuity')
+				userDir += connection.username
+		}
+		else
+		{
+			userDir = '/'
+		}
 
-			//Parse userDir, from config file and username
-			var userDir = config.basics.root
-			if (config.basics.root)
+		// If the directory exists, call back immediately
+		// with that directiory
+		// If not, create the directiory and callback
+		// possible error and the directory
+		fs.exists(userDir, function(exists)
+		{
+			if (exists)
 			{
-				if (connection.username != 'ingenuity')
-					userDir += connection.username
+				callback(null, userDir)
 			}
 			else
 			{
-				userDir = '/'
-			}
-			//If the directory exists, call back immediately with that directiory
-			//If not, create the directiory and callback possible error and the directory
-			fs.exists(userDir, function(exists)
-			{
-				if(exists)
+				fs.mkdir(userDir, function(err)
 				{
-					callback(null, userDir)
-				}
-				else
-				{
-					fs.mkdir(userDir, function(err)
-					{
-						callback(err, userDir)
-					})
-				}
-			})
-		},
-		//No callback, get root directory for the user
-		getRoot: function ()
-		{
-			//From user's standpoint, this cwd will appear as '/'
-			if (config.basics.root)
-			{
-				return '/'
+					callback(err, userDir)
+				})
 			}
-			else
-			{
-				return process.cwd()
-			}
-		},
+		})
+	},
 
-		//TODO: for testing
-		/*getInitialCwd: function (connection) {
-			//return '/';
-			return '/files/'
-		},
-		getRoot: function () {
-			console.log(process.cwd())
-			var path = process.cwd()
-			console.log(path)
-			path += '/files/'
-			return path
-		}*/
-	})
+	// No callback, get root directory for the user
+	getRoot: function ()
+	{
+		// From user's standpoint, this cwd will appear as '/'
+		if (config.basics.root)
+			return '/'
+		else
+			return process.cwd()
+	},
 
-//Handle errors, if any
+	// TODO: for testing
+	// getInitialCwd: function (connection) {
+	// 	//return '/'
+	// 	return '/files/'
+	// },
+	// getRoot: function () {
+	// 	console.log(process.cwd())
+	// 	var path = process.cwd()
+	// 	console.log(path)
+	// 	path += '/files/'
+	// 	return path
+	// }
+})
+
+// Handle errors, if any
 server.on('error', function (error)
 {
 	console.log('FTP Server error:', error)
 })
 
-//Initiate connection, with authentication
+// Initiate connection, with authentication
 server.on('client:connected', function (connection)
 {
 	var username = null
 	console.log('client connected: ' + connection.remoteAddress)
 	connection.on('command:user', function (user, success, failure)
 	{
-		//If user found in config.users collection, continue
+		// If user found in config.users collection, continue
 		if(_.find(config.users, {'username': user}))
 		{
 			username = user
@@ -111,14 +113,19 @@ server.on('client:connected', function (connection)
 			failure()
 			var err = new Error('Error: Username not found')
 			console.log(err.message)
-			process.exit(1) //exit with failure
+			// exit with failure
+			process.exit(1)
 		}
 	})
 
 	connection.on('command:pass', function (pass, success, failure)
 	{
-		//If user has correct corresponding password in config.users collection, continue
-		if(_.find(config.users, {'username':username, 'password':pass}))
+		// If user has correct corresponding password
+		// in config.users collection, continue
+		var matchingUser = _.find(config.users,
+			{'username':username, 'password':pass})
+
+		if (matchingUser)
 		{
 			success(username)
 		}
@@ -132,25 +139,19 @@ server.on('client:connected', function (connection)
 	})
 })
 
-server.debugging = 4;
+server.debugging = 4
 
 server.listen(options.port)
 console.log('Listening on port ' + options.port)
 
-/*Helper functions*/
+// Helper functions
 
-/*
-Function: userExists
-Purpose: given a collection and a username, see if username is in that collection
-Input: collection - collection of objects to search, user - string username to find
-Output: boolean - true if found, false if not
-*/
-/*function userExists(collection, field, user)
-{
-	if(_.find(collection, {field: user})){
-		return true
-	}else{
-		return false
-	}
-}*/
-
+// Function: userExists
+// Purpose: given a collection and a username, see if username is in that collection
+// Input: collection - collection of objects to search, user -
+// string username to find
+// Output: boolean - true if found, false if not
+// function userExists(collection, field, user)
+// {
+// 	return _.find(collection, {field: user})
+// }
--- config/default.js
+++ config/default.js
@@ -1,33 +1,36 @@
 module.exports = {
 
 	basics: {
-		//Use enviornment vars IP and PORT if they exist, else use defaults
+		// Use enviornment vars IP and PORT if they exist, else use defaults
 		host: process.env.IP || '127.0.0.1',
 		port: process.env.PORT || 7002,
-		//Must be a subdirectory of where the server is run from
-		root: '/ie/cloudFTP/files/', //On instance, include /var/ftp/files
-		//TODO: alternatively, /files
+		// Must be a subdirectory of where the server is run from
+		// On instance, include /var/ftp/files
+		root: '/ie/cloudFTP/files/',
+		// TODO: alternatively, /files
 	},
-	/*users:[
+	// users:[
+	// 	{
+	// 		name: 'Kenji Endo',
+	// 		username: 'ckendo',
+	// 		email: 'colvin_endo@brown.edu',
+	// 	},
+	// 	{
+	// 		name: 'Grant Miller',
+	// 		username: 'blented',
+	// 		email: 'blented@gmail.com',
+	// 	},
+	// ],
+	users: [
 		{
-			name: 'Kenji Endo',
-			username: 'ckendo',
-			email: 'colvin_endo@brown.edu',
-		},
-		{
-			name: 'Grant Miller',
-			username: 'blented',
-			email: 'blented@gmail.com',
-		},
-	],*/
-	users:[
-		{
-			//Ingenuity admin account, with access to all of file directory
+			// Ingenuity admin account, with access to
+			// all of file directory
 			name: 'ingenuity',
 			username: 'ingenuity',
 			password: 'ingenuity',
 		},
-			//User accounts will only have access to /files/[USER]
+			// User accounts will only have access to
+			// /files/[name]
 		{
 			name: 'brooklyn_99',
 			username: 'b99',
@@ -40,5 +43,5 @@ module.exports = {
 		},
 	],
 
-//end of config
+// end of config
 }
